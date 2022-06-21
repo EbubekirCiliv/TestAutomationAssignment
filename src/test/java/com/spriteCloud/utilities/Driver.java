e201e53b1b70fd3df7064c32301479094077c2ec
EbubekirCiliv <72078134+EbubekirCiliv@users.noreply.github.com>
2022-06-21 12:47:21 +0200

    update driver

--- src/test/java/com/spriteCloud/utilities/Driver.java
+++ src/test/java/com/spriteCloud/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.spriteCloud.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -23,29 +24,37 @@ public class Driver {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
-                    Driver.get().manage().window().maximize();
                     break;
 
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
-                    Driver.get().manage().window().maximize();
                     break;
 
                 case "edge":
                     WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
-                    Driver.get().manage().window().maximize();
                     break;
 
                 case "safari":
                     WebDriverManager.getInstance(SafariDriver.class).setup();
                     driver = new SafariDriver();
-                    Driver.get().manage().window().maximize();
+                    break;
 
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--window-size=1920,1080");
+                    options.addArguments("--disable-gpu");
+                    options.addArguments("--disable-extensions");
+                    options.addArguments("--proxy-server='direct://'");
+                    options.addArguments("--proxy-bypass-list=*");
+                    options.addArguments("--start-maximized");
+                    options.addArguments("--headless");
+                    driver = new ChromeDriver(options);
                     break;
             }
-
+            Driver.get().manage().window().maximize();
         }
 
         return driver;
